1fcad1be2a60630a84a9b92bc57767a8b7f117ac
paulvchow <paulvchow@gmail.com>
2017-03-04 12:53:26 -0800

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,30 @@
+//This is the navbar js to make it stick to the top of the page after scrolling down the home pic
+$(function() {
+    var nav = $('#nav');
+    var navHomeY = nav.offset().top;
+    var isFixed = false;
+    var $w = $(window);
+    $w.scroll(function() {
+        var scrollTop = $w.scrollTop();
+        var shouldBeFixed = scrollTop > navHomeY;
+        if (shouldBeFixed && !isFixed) {
+            nav.css({
+                position: 'fixed',
+                top: 0,
+                left: nav.offset().left,
+                width: nav.width()
+            });
+            isFixed = true;
+        }
+        else if (!shouldBeFixed && isFixed)
+        {
+            nav.css({
+                position: 'static'
+            });
+            isFixed = false;
+        }
+    });
+});
+
+//**********************************************************
+
